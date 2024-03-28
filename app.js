32cf1887b73b60c405902d37c4061c97ac329662
wrfmartinez <wrmartinez709@gmail.com>
2024-03-28 14:49:50 -0400

    Concatenate the strings in the 'foods' array separated by ' -> '

--- app.js
+++ app.js
@@ -103,4 +103,22 @@ Complete Exercise 8 in the space below:
 
 let soyIdx = foods.indexOf('tofu');
 
-console.log('Exercise 8 result:', soyIdx);
+// console.log('Exercise 8 result:', soyIdx);
+
+/*
+Exercise 9: Joining elements
+
+1) Use the `join()` method to concatenate the strings in the `foods` array, 
+   separated by ' -> '. 
+
+2) Assign the result to a variable called `allFoods`. 
+
+Note: The final result should log as:
+'taco -> sushi -> cupcake -> tofu -> cheeseburger'
+
+Complete Exercise 9 in the space below:
+*/
+
+const allFoods = foods.join(' -> ');
+
+console.log('Exercise 9 result:', allFoods);
