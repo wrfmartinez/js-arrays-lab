411a402a9b626638c80d5843678e800fec9a2fb3
wrfmartinez <wrmartinez709@gmail.com>
2024-03-28 14:51:50 -0400

    Check if the 'foods' array contains 'soup'

--- app.js
+++ app.js
@@ -121,4 +121,19 @@ Complete Exercise 9 in the space below:
 
 const allFoods = foods.join(' -> ');
 
-console.log('Exercise 9 result:', allFoods);
+// console.log('Exercise 9 result:', allFoods);
+
+/*
+Exercise 10: Check for an element
+
+1) Using the .includes() method, check if the `foods` array contains the string
+   'soup'.
+
+2) Assign the result to a variable called `hasSoup``.
+
+Complete Exercise 10 in the space below:
+*/
+
+const hasSoup = foods.includes('soup');
+
+console.log('Exercise 10 result:', hasSoup);
