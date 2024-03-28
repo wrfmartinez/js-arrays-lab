5015f539d0509cff8b2524aa58aa9a1d8a1d6d58
wrfmartinez <wrmartinez709@gmail.com>
2024-03-28 14:56:51 -0400

    Push odd numbers from 'nums' array into a new array called 'odds'

--- app.js
+++ app.js
@@ -136,4 +136,28 @@ Complete Exercise 10 in the space below:
 
 const hasSoup = foods.includes('soup');
 
-console.log('Exercise 10 result:', hasSoup);
+// console.log('Exercise 10 result:', hasSoup);
+
+/*
+Exercise 11: Odd numbers from an array
+
+1) Choose a method to iterate through the `nums` array.
+
+2) Push each odd number to a new array named `odds`.
+
+Hint: Initialize the `odds` variable to an empty array before the iteration.
+
+Complete Exercise 11 in the space below:
+*/
+
+const nums = [100, 5, 23, 15, 21, 72, 9, 45, 66, 7, 81, 90];
+
+let odds = [];
+
+for (const number of nums) {
+  if (number % 2 != 0) {
+    odds.push(number);
+  }
+}
+
+console.log('Exercise 11 result:', odds);
