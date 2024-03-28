b80aecded7a5c8732e7e1b53fbe9a8131432bbf0
wrfmartinez <wrmartinez709@gmail.com>
2024-03-28 14:26:14 -0400

    Create empty foods array and add two items to the array

---
+++ app.js
@@ -0,0 +1,25 @@
+/*
+Exercise 1: Define an empty array
+
+1) Create an empty array and assign it to a variable called `foods`.
+
+Exercise 1 has been completed for you:
+*/
+
+const foods = [];  
+
+// console.log('Exercise 1 result:', foods);
+
+/*
+Exercise 2: Add strings to the array
+
+1) Add 'pizza' and 'cheeseburger' to the `foods` array. 
+
+Note: 'pizza' should be the first item in the array, followed by 'cheeseburger'.
+
+Complete Exercise 2 in the space below:
+*/
+
+foods.push('pizza', 'cheeseburger');
+
+console.log('Exercise 2 result:', foods);
