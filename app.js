2d62bccb064590a4abbb5a4c9d7ba29caf69de82
wrfmartinez <wrmartinez709@gmail.com>
2024-03-28 14:31:20 -0400

    Assign pizza to a variable called favFood

--- app.js
+++ app.js
@@ -34,4 +34,19 @@ Complete Exercise 3 in the space below:
 
 foods.unshift('taco');
 
-console.log('Exercise 3 result:', foods);
+// console.log('Exercise 3 result:', foods);
+
+/*
+Exercise 4: Access an array element
+
+1) Retrieve the 'pizza' string from the array based on its position (index) in
+   the array.  
+
+2) Assign it to a variable called `favFood`.
+
+Complete Exercise 4 in the space below:
+*/
+
+let favFood = foods[1];
+
+console.log('Exercise 4 result:', favFood);
