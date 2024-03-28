5974e0566624965c4548e0a343cd405771e11848
wrfmartinez <wrmartinez709@gmail.com>
2024-03-28 14:36:13 -0400

    Insert the string tofu between pizza and cheeseburger in the foods array

--- app.js
+++ app.js
@@ -49,4 +49,16 @@ Complete Exercise 4 in the space below:
 
 let favFood = foods[1];
 
-console.log('Exercise 4 result:', favFood);
+// console.log('Exercise 4 result:', favFood);
+
+/*
+Exercise 5: Insert an element between two others
+
+1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
+
+Complete Exercise 5 in the space below:
+*/
+
+foods.splice(2, 0, 'tofu');
+
+console.log('Exercise 5 result:', foods);
